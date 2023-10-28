a69c5e7767662380332c446e5600f8f1a7bdef1e
Gabriel-Paes <paessouzagabriel@gmail.com>
2023-10-28 14:42:25 -0300

    add class Book

--- bookstore/src/main/java/com/bookstore/App.java
+++ bookstore/src/main/java/com/bookstore/App.java
@@ -2,6 +2,12 @@ package com.bookstore;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Biblioteca\n");
+
+        Book book1 = new Book("Arquitetura Limpa", "Robert C. Martin", "978-85-508-0460-6", true);
+        book1.printBook();
+
+        Book book2 = new Book("Ahsoka", "E. K. Johnston", "978-65-5609-027-6", true);
+        book2.printBook();
     }
 }
