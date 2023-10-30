b2b93de59c0917cb164b05c2a59d69a1da67c02f
Gabriel-Paes <paessouzagabriel@gmail.com>
2023-10-30 17:23:49 -0300

    add class LinkedList and Nodule

--- bookstore/src/main/java/com/bookstore/App.java
+++ bookstore/src/main/java/com/bookstore/App.java
@@ -1,13 +1,29 @@
 package com.bookstore;
 
+import com.bookstore.linkedlist.LinkedList;
+
 public class App {
     public static void main(String[] args) {
+        LinkedList bookshelf = new LinkedList();
+
+        bookshelf.push(new Book("Arquitetura Limpa", "Robert C. Martin", "978-85-508-0460-6", true));
+        bookshelf.push(new Book("Ahsoka", "E. K. Johnston", "978-65-5609-027-6", true));
+
+        System.out.println(bookshelf.length());
+        System.out.println(bookshelf);
+
         System.out.println("Biblioteca\n");
 
-        Book book1 = new Book("Arquitetura Limpa", "Robert C. Martin", "978-85-508-0460-6", true);
-        book1.printBook();
+        Book bookSearched = bookshelf.getBookByAuthor("Robert C. Martin");
+        bookSearched.printBook();
 
-        Book book2 = new Book("Ahsoka", "E. K. Johnston", "978-65-5609-027-6", true);
-        book2.printBook();
+        bookshelf.remove("978-85-508-0460-6");
+
+        Book bookSearched2 = bookshelf.getBookByAuthor("Robert C. Martin");
+        bookSearched2.printBook();
+
+        System.out.println(bookshelf);
+        System.out.println(bookshelf.length());
+
     }
 }
